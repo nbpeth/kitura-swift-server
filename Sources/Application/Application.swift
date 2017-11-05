4b5e1d7cdf4f09f0eea4d8c2053e5b7e975efb04
bud manstrong <nbpeth@gmail.com>
2017-11-05 17:49:44 -0600

    get endpoint configured

--- Sources/Application/Application.swift
+++ Sources/Application/Application.swift
@@ -10,11 +10,22 @@ import Health
 public let projectPath = ConfigurationManager.BasePath.project.path
 public let health = Health()
 
+class Robot: Codable {
+    
+    var name:String?
+    var modelNumber:String?
+    var batteryLevel:Float?
+    var job:String?
+    
+}
+
 public class App {
     let router = Router()
     let cloudEnv = CloudEnv()
     let swaggerPath = projectPath + "/definitions/exmaple-swift-server.yaml"
 
+    var robots = [Robot]()
+    
     public init() throws {
     }
 
@@ -24,13 +35,21 @@ public class App {
 
         // Middleware
         router.all(middleware: StaticFileServer())
-
+        router.get("/", handler: getHandler)
         // Endpoints
-        
         
         try initializeCRUDResources(cloudEnv: cloudEnv, router: router)
         initializeHealthRoutes(app: self)
         initializeSwaggerRoutes(app: self)
+    }
+    
+    func getHandler(completion: ([Robot]?, RequestError?) -> Void ) -> Void {
+        let robot = Robot()
+        robot.batteryLevel = 0.78
+        robot.job = "War Bot"
+        robot.modelNumber = "XZBB12"
+        robots.append(robot)
+        completion(robots, nil)
     }
 
     public func run() throws {
